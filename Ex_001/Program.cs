063dcf57cd340efd11122e8e0d2ed69afff4bd59
DBarrett <darryonbarrett@gmail.com>
2023-06-23 22:19:45 +0300

    Finished work on 001 & 002

---
+++ Ex_001/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int InputSumNums()
+{
+    Console.WriteLine("Введите число: ");
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int InputGradeNum()
+{
+    Console.WriteLine("Введите степень числа: ");
+    int gradeNum = int.Parse(Console.ReadLine()!);
+    return gradeNum;
+}
+
+int Grading(int number, int gradeNum)
+{
+    int result = number;
+    for (int i = 1; i < gradeNum; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+
+int num = InputSumNums();
+int grade = InputGradeNum();
+int result = Grading(num, grade);
+Console.WriteLine(result);
---
+++ Ex_002/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на 
+//вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNum()
+{
+    Console.WriteLine("Введите число: ");
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int SlicingAndSum(int number)
+{
+    int slicedNum = 0;
+    while (number != 0)
+    {
+        int reminder = number % 10;
+        number /= 10;
+        slicedNum += reminder;
+    }
+    return slicedNum;
+}
+
+
+int num = InputNum();
+int sum = SlicingAndSum(num);
+Console.WriteLine(sum);
